cb6f8c297f8f362ea8294d3d6fb87f8bc4b7c488
IgroRik <48213289+IgroRik@users.noreply.github.com>
2020-09-09 19:53:50 +0300

    Add models Film Actor Connect

--- Actors/models.py
+++ Actors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    Name = models.CharField('FilmName', max_length=100)
+    Year = models.IntegerField('YearOfCreation')
+
+
+class Actor(models.Model):
+    Name = models.CharField('ActorName', max_length=100)
+    Year = models.IntegerField('YearOfBirth')
+
+
+class FilmActor(models.Model):
+    FilmId = models.ForeignKey(Film, on_delete=models.CASCADE)
+    ActorId = models.ForeignKey(Actor, on_delete=models.CASCADE)
