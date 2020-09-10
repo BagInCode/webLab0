660d504a89a6288f565d67ab9ef71a808629d6a5
IgroRik <48213289+IgroRik@users.noreply.github.com>
2020-09-10 14:51:46 +0300

    Commit link review

--- Actors/models.py
+++ Actors/models.py
@@ -4,11 +4,18 @@ from django.db import models
 class Film(models.Model):
     Name = models.CharField('FilmName', max_length=100)
     Year = models.IntegerField('YearOfCreation')
+    Poster = models.ImageField(upload_to='Films')
+
+    def __str__(self):
+        return self.Name
 
 
 class Actor(models.Model):
     Name = models.CharField('ActorName', max_length=100)
     Year = models.IntegerField('YearOfBirth')
+
+    def __str__(self):
+        return self.Name
 
 
 class FilmActor(models.Model):
